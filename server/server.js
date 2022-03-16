0837ae5c75f46083f3ab68cd60acb5d002c38d04
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-16 10:58:41 +0100

    init

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const cors = require("cors")
+const favicon = require('serve-favicon');
 
 app.use(cors());
 app.use(express.json())
-
+app.use(favicon(__dirname + '/build/favicon.ico'))
 app.use(express.static(path.join(__dirname, "build")))
 app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/build/index.html"))
+    res.sendFile(path.join(__dirname, "./build/index.html"))
 })
 
 
