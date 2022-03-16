13580d11edfeca66a35d56f57fc7ab31d20ed67c
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-16 13:20:54 +0100

    add react app

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css"
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+
+    render(){
+
+        return(
+            <div className="app">
+                <h1>hello world</h1>
+            </div>
+        )
+    }
+}
+export default App;
