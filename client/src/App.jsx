fdcb105edfb19806a0eb1ce6a1becc422e4b00bf
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-19 20:12:42 +0100

    panel logowania

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import MainView from "./components/mainView/MainView";
 import Regestry from "./components/regestry/Regestry";
+import Login from "./components/login/Login";
 import "./App.css";
 
 class App extends Component {
@@ -9,12 +10,14 @@ class App extends Component {
     this.state = {
       mainWindow: true,
       regestry: false,
+      login: false
     };
   }
   handleOpenRegestry = () => {
     const mainWindow = this.state;
     if (mainWindow) {
       this.setState({ mainWindow: false });
+      this.setState({login:false})
       this.setState({ regestry: true });
     }
   };
@@ -22,22 +25,36 @@ class App extends Component {
     const regestry = this.state;
     if (regestry) {
       this.setState({ mainWindow: true });
+      this.setState({login:false})
       this.setState({ regestry: false });
     }
   };
+  handleOpenLogin = () => {
+      const mainWindow = this.state
+      const regestry = this.state
+      if(mainWindow){
+          this.setState({mainWindow: false});
+          this.setState({regestry: false})
+          this.setState({login: true})
+      }
+  }
   render() {
     const { mainWindow } = this.state;
     const { regestry } = this.state;
+    const {login} = this.state
     return (
       <div className="app">
         {mainWindow && (
-          <MainView openRegestry={() => this.handleOpenRegestry()} />
+          <MainView openRegestry={() => this.handleOpenRegestry()} 
+                    openLogin={() => this.handleOpenLogin()}/>
         )}
         {regestry && (
           <Regestry colseRegestry={() => this.handleCloseRegestry()} />
         )}
+        {login && 
+         <Login />}
       </div>
-    );
+    )
   }
 }
 export default App;
