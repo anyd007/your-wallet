2cf600c557e21e558a22eafa59b014ce1d699156
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-29 18:22:11 +0200

    szkielet panelu wydatków

--- client/src/components/outcome-database/OutcomeDataBase.jsx
+++ client/src/components/outcome-database/OutcomeDataBase.jsx
@@ -2,20 +2,55 @@ import React from "react";
 import axios from "axios";
 import {CgCloseR} from "react-icons/cg"
 import "./outcomeDataBase.css";
+import { Zoom } from "react-reveal";
 
 const outcomeDataBase = (props) => {
 
   return (
-    <div className="outcomeMainContener">
+  <div className="outcomeMainContener">
       <CgCloseR onClick={() => props.closeOutcomeDatabase()} className="closeIcon" />
       <div className="databaseBackground"></div>
-      <div className="titleGroup">
-        <h2 className="databaseTitle">TWOJE FINANSOWE DANE</h2>
-        <p className="databaseInfo">
-          wprowadzaj przychody oraz wydatki, planuj, oszczędzaj...
-        </p>
-      </div>
-    </div>
+    <section className="titleGroup">
+        <h2 className="databaseTitle"><Zoom left cascade>TWOJE FINANSOWE DANE</Zoom></h2>
+        <Zoom><p className="databaseInfo">
+          wprowadzaj przychody oraz wydatki, planuj, oszczędzaj...</p></Zoom>
+    </section>
+    <section className="outComeGroup">
+     <Zoom><div className="outcomeInputs" >
+          <label htmlFor="outcome">WPROWADŹ KWOTĘ WYDATKU</label>
+          <input 
+          id="outcome"
+          className="inputsFeelds"
+          name="outcome"
+          type="number"
+          min="1"/>
+        </div></Zoom>
+       <Zoom><div className="outcomeInputs">  
+          <label htmlFor="outcome-choose">WYBIERZ RODZAJ WYDATKU</label>
+          <input
+          id="outcome-choose"
+          className="inputsFeelds"
+          name="outcome-choose"
+          type="text"/>
+        </div></Zoom>
+       <Zoom><div className="outcomeInputs" >
+          <label htmlFor="outcome-date">WPROWADŹ DATE WYDATKU</label>
+          <input 
+          id="outcome-date"
+          className="inputsFeelds"
+          name="outcome-date"
+          type="date"/>
+        </div></Zoom>
+        <Zoom><div className="outcomeInputs" >
+          <label htmlFor="outcome-comment">KOMENTARZ (max 100 znaków)</label>
+          <textarea 
+          id="outcome-comment"
+          className="inputsFeelds"
+          name="outcome-comment"
+          maxLength="100"/>
+        </div></Zoom>
+    </section>
+  </div>
   );
 };
 
